8975e489bff1db4f64f168e38129ebde1c3bc4ce
Michou <Michou@com>
2024-12-24 15:47:09 +0100

    [add] kafka integration

---
+++ src/cli/audio_kafka_producer.py
@@ -0,0 +1,76 @@
+import argparse
+import traceback
+from typing import Union
+import rx.operators as ops
+
+from src.sources.audio import MicrophoneAudioSourceTimed, FileAudioSourceTimed
+from src.producer.base import BaseProducer
+from src.producer.audio import KafkaAudioProducer
+from src.schema.config.audio import KafkaAudioConfig
+
+
+def main(producer: BaseProducer, source_audio: Union[MicrophoneAudioSourceTimed, FileAudioSourceTimed]):
+    source_audio.stream.pipe(ops.do(producer)).subscribe(
+        on_error=lambda _: traceback.print_exc()
+    )
+    source_audio.read()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Audio Kafka Producer Script")
+    parser.add_argument(
+        "--topic",
+        type=str,
+        default="audio",
+        help="Name of the Kafka topic (default: 'audio')",
+    )
+    parser.add_argument(
+        "--bootstrap-servers",
+        type=str,
+        default="localhost:29092",
+        help="Kafka bootstrap servers (default: 'localhost:29092')",
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        choices=["microphone", "file"],
+        default="microphone",
+        help="Audio source: 'microphone' or 'file' (default: 'microphone')",
+    )
+    parser.add_argument(
+        "--block-duration",
+        type=int,
+        default=2,
+        help="Block duration in seconds for audio reading (default: 2)",
+    )
+    parser.add_argument(
+        "--device",
+        type=int,
+        default=None,
+        help="Microphone device ID to use (default: first available)",
+    )
+    parser.add_argument(
+        "--file-path",
+        type=str,
+        default=None,
+        help="Path to the audio file if source='file'",
+    )
+    args = parser.parse_args()
+
+    producer = KafkaAudioProducer(
+        kafka_config=KafkaAudioConfig(
+            topic=args.topic, bootstrap_servers=args.bootstrap_servers
+        )
+    )
+
+    if args.source == "microphone":
+        source_audio = MicrophoneAudioSourceTimed(
+            block_duration=args.block_duration, device=args.device
+        )
+    elif args.source == "file":
+        if not args.file_path:
+            raise ValueError(
+                "An audio file must be specified with --file-path.")
+        source_audio = FileAudioSourceTimed(file_path=args.file_path)
+
+    main(producer, source_audio=source_audio)
